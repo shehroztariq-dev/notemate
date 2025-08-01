f1e4c5324d6a8c1e72d15f4d6b24b316daebd384
Shehroz Tariq <shehroztariq.dev@gmail.com>
2025-08-01 15:29:25 +0500

    Production Ready

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:5000/api" });
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "api";
+
+const api = axios.create({ baseURL: BASE_URL });
 
 export default api;
--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -10,18 +11,29 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+const __dirname = path.resolve();
 // middleware
 app.use(express.json());
 app.use(ratelimiter);
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 
 // Routes
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+
+  app.all("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
